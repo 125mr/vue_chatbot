64976c834718b82a4873a364e153dbf5bcd1504d
125mr <r.murakami@engenius.co.jp>
2023-06-26 22:02:12 +0900

    vueの使い方をおさらい

---
+++ src/components/TestComponent.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+      (名前なし)村上璃来
+    </div>
+
+    <p>{{ message }}</p>
+    <!-- <p>{{ isEnabled }}</p> -->
+    <button @click="onClick">親メソッドを実行</button>
+  </template>
+
+
+
+<script>
+  export default {
+    props: ['message'],
+    methods: {
+      onClick() {
+        this.$emit('toggle')
+      }
+    }
+
+  }
+
+
+</script>
---
+++ src/views/WelcomePage.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <h1>{{ title }}</h1>
+    </div>
+    <h1>
+        <TestComponent @toggle="toggle" message="Componentにメッセージが渡されています"/>
+        <p v-if="isEnabled"></p>
+    </h1>
+</template>
+
+<script>
+
+import TestComponent from '../components/TestComponent.vue'
+
+export default {
+
+  components: {
+    TestComponent
+  },
+
+  data () {
+    return {
+      title: 'コンポーネントから読み込んだ充電器',
+      subtitle: 'TYPEーC',
+      isEnabled: true
+    }
+  },
+  computed: {
+    text() {
+        if(this.isEnabled) {
+            return 'trueの値だよ'
+        } else {
+            return 'falseの値だよ'
+        }
+    }
+  },
+  methods: {
+    toggle () {
+        this.isEnabled = !this.isEnabled
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+  p {
+    font-size: 20px;
+    color: red;
+  }
+</style>
